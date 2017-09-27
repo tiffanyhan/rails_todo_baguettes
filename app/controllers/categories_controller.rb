3f928f112d5ff00b739ce41a9936c3eb782ef732
Jay Shenk <jay.shenk@gmail.com>
2017-09-26 21:52:51 -0400

    visit bakery and complete authorization

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,7 @@
 class CategoriesController < ApplicationController
+  before_action :require_login, only: [:new, :create, :show]
+  before_action :correct_user, only: [:show]
+
   def new
     @category = Category.new
   end
@@ -16,11 +19,21 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
+    @incomplete_todos = @category.todos.where(completed: false, user_id: current_user.id).order(due_date: :asc)
+    @complete_todos = @category.todos.where(completed: true, user_id: current_user.id).order(due_date: :asc)
   end
 
   private
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def correct_user
+    if Category.find(params[:id]).user != current_user
+      flash[:error] = "You went the wrong way. Authorized employees only."
+      redirect_to root_path
+    end
   end
 end
